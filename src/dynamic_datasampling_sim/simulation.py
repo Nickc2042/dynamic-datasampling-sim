d43250b9427d4fecaab5e2f59636ff14258007f1
Nickc2042 <nickc2042@gmail.com>
2026-06-05 10:10:23 -0500

    Refactor run_simulation to support simulation_config for improved flexibility and maintainability

--- src/dynamic_datasampling_sim/simulation.py
+++ src/dynamic_datasampling_sim/simulation.py
@@ -6,19 +6,36 @@ from .sampling import get_sample, update_environ, update_strat
 from .strategy import strategy
 
 
-def run_simulation(strattype=None, environtype=None, T=None):
-    if strattype is None:
-        strattype = config.default_strategy_type # View simulation_config.py for details on any "config." variables
-    if environtype is None:
-        environtype = config.default_environment_type # defaults.py may be added in future updates for cleaner code
-    if T is None:
-        T = config.default_simulation_time
+def run_simulation(strattype = None, environtype = None, T = None, simulation_config = None):
+    # From simulation_config or the so called "default/legacy"
+    if simulation_config is not None:
+        if strattype is None:
+            strattype = simulation_config.default_strategy_type
+        if environtype is None:
+            environtype = simulation_config.default_environment_type
+        if T is None:
+            T = simulation_config.default_simulation_time
 
-    config.validate_config(simulation_time=T)
+        simulation_config.validate(simulation_time = T)
+        strategy_config = simulation_config.strategy
+        environment_config = simulation_config.environment
+    
+    # From defaults if not provided in simulation_config
+    else:
+        if strattype is None:
+            strattype = config.default_strategy_type # View simulation_config.py for details on any "config" variables
+        if environtype is None:
+            environtype = config.default_environment_type # defaults.py may be added in future updates for cleaner code
+        if T is None:
+            T = config.default_simulation_time
+
+        config.validate_config(simulation_time = T)
+        strategy_config = None
+        environment_config = None
 
     # Strategy & environment initialization based on earlier choices
-    strat = strategy(strattype=strattype)
-    environ = environment(environtype=environtype)
+    strat = strategy(strattype = strattype, strategy_config = strategy_config)
+    environ = environment(environtype =environtype, environment_config = environment_config)
 
     # initialization for time index
     t = 0
