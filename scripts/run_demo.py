b75ce2e6bb03a5a882f47f4514d4cf2716692580
Nickc2042 <nickc2042@gmail.com>
2026-06-05 10:49:10 -0500

    Add command-line argument support for simulation config and create example YAML config file

--- scripts/run_demo.py
+++ scripts/run_demo.py
@@ -1,15 +1,26 @@
 from pathlib import Path
 import sys
+import argparse
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 sys.path.insert(0, str(PROJECT_ROOT))
 
+from src.dynamic_datasampling_sim.config_loader import load_config
 from src.dynamic_datasampling_sim.simulation import run_simulation
 
 
 def main():
-    times, states, samples, costs = run_simulation()
+    parser = argparse.ArgumentParser() # Parse and allows uerse to specify a config file when running the demo
+    parser.add_argument(
+        "--config",
+        default = PROJECT_ROOT / "configs" / "modulation.yml",
+        help = "Path to a YAML simulation config file.",
+    )
+    args = parser.parse_args()
+
+    simulation_config = load_config(args.config)
+    times, states, samples, costs = run_simulation(simulation_config = simulation_config)
 
     print("Sample Times: ")
     print(times)
